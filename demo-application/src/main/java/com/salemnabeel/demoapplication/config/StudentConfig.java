27f292e6a4b5047fc73804b115f9607f70b57506
Salem Nabeel Salem <samnabeelsam@outlook.com>
2021-01-16 16:09:00 +0300

    create the student config class for allow adding some of students data

---
+++ demo-application/src/main/java/com/salemnabeel/demoapplication/config/StudentConfig.java
@@ -0,0 +1,43 @@
+package com.salemnabeel.demoapplication.config;
+
+import com.salemnabeel.demoapplication.model.Student;
+import com.salemnabeel.demoapplication.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student salem = new Student(
+                "Salem Nabeel",
+                "salemnabeel@gmail.com",
+                LocalDate.of(1994, Month.SEPTEMBER, 01),
+                26
+            );
+
+            Student ammar = new Student(
+                "Ammar Ali",
+                "ammarali@outlook.com",
+                LocalDate.of(1995, Month.APRIL, 02),
+                25
+            );
+
+            Student ali = new Student(
+                "Ali Omer",
+                "aliomer@hotmail.com",
+                LocalDate.of(1996, Month.MAY, 03),
+                24
+            );
+
+            studentRepository.saveAll(List.of(salem, ammar, ali));
+        };
+    }
+}
